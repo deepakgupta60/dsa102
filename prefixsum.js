c19478a1793a0804cb3fe8eceb413ce52e15fcd5
Deepak Gupta <deepakgupta60k@gmail.com>
2024-11-03 09:22:35 +0530

    working

---
+++ prefixsum.js
@@ -0,0 +1,24 @@
+(function main(){
+    let ans = prefixSum()
+    console.log("Answer is: ", ans)
+  }())
+  
+  function prefixSum()
+  {
+    let arr=[1,2,3,4];
+    let prefix=new Array(arr.length)
+    for(let i=0;i<arr.length;i++)
+      {
+        if(i==0)
+          {
+            prefix[i]=arr[i]
+          }
+        else
+          {
+            prefix[i]=arr[i]+prefix[i-1]
+          }
+      }
+  
+    return prefix
+  }
+  
