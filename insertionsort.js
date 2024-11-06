34330f3dfc899d04ca2450643951334454196bea
Deepak Gupta <deepakgupta60k@gmail.com>
2024-11-07 04:26:42 +0530

    working

---
+++ insertionsort.js
@@ -0,0 +1,20 @@
+// // Optimal /
+// // might have heard or might not have heard..
+// // that insertion sort() // what do..
+
+// let arr = [5,4,1,3,2]
+// insertion sort always start from i=1, everything before 4 is lesser then, and everything after 4 is greater then 4
+
+// [23,1,10,5,2]
+
+// i=1, element=1, then it will be swap > [1,23, 10, 5, 2]
+
+// i=2, element=10, then it will be swap with before that element..
+// [1,10,23,5,2]
+
+// i=3, element=5, then it will be swap with prev element, [1,10,5,23,2], [1,5,10,23,2]
+
+// i=4, element=2, then it will be swap with prev elmenet [1,5,10,2,23], [1,5,2,10,23]
+
+
+
