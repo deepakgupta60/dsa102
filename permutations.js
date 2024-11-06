b7e036dded75adba660f230c26ff06dfd7358e1f
Deepak Gupta <deepakgupta60k@gmail.com>
2024-11-07 00:14:50 +0530

    working

---
+++ permutations.js
@@ -0,0 +1,22 @@
+// // Permutations..
+
+// Find if the string permutations can form a palindrome
+
+// "nnaamm" this is not a palindrome but when re arrange then it should be a palindrom..
+// > "mnaanm"
+
+// "racecar" > 'r':2, 'a':2, 'c':2,'e':1
+
+// if(number of character with odd frequency <=1) then return true, else return false
+
+// 'a':4, 'b':2,'c':4
+// 'a':4, 'b':3,'c':4
+
+
+// NOTE: not more than one character is odd
+// any string with even length > with odd freq with 0
+
+// aabcbaa > yes palindrome
+// aabcbbaa > no palindrome --> only one odd is allowed..
+
+
