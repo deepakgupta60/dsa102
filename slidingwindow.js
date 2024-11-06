9b0f566434133c26a0095222d833d8c6b858f580
Deepak Gupta <deepakgupta60k@gmail.com>
2024-11-06 16:37:30 +0530

    sliding window

--- slidingwindow.js
+++ slidingwindow.js
@@ -1,27 +1,30 @@
-(function main()
-{
-  let ans = sliding();
-  console.log("answer: ",ans)
+(function main(){
+  const ans = slidingWindow();
+  console.log("Answer: ", ans)
 }())
 
-function sliding()
+function slidingWindow()
 {
-  let arr = [2,5,9,4,3,1]
-  
+  let arr = [2,5,9,2,5,4,8,9,3];
+  let n=arr.length;
   let k=3
-  let left=0, right=k-1; // first window
   let currSum=0;
-  let n=arr.length
-  for(let i=left;i<=right;i++)
-  {
-    currSum+=arr[i]
-  }
-  let maxSum =currSum;
-  while(right<=n-2)
+  
+  for(let i=0;i<k;i++)
+    {
+      currSum+=arr[i]
+    }
+  let maxSum = currSum
+  
+  let left=0, right=k-1;
+  
+  while(right<n-1)
     {
       left++
       right++
       currSum+=arr[right]-arr[left-1]
+      
+      console.log(currSum)
       if(maxSum<currSum)
         {
           maxSum=currSum
