e1a5fee49b1620a5d14b8f514911e6bb1d9bcee0
Deepak Gupta <deepakgupta60k@gmail.com>
2024-11-07 00:20:04 +0530

    working

---
+++ isPermutations.js
@@ -0,0 +1,26 @@
+function isPermutations()
+{
+  let s="madam"
+  let st = new Set()
+  for(let i=0;i<s.length;i++)
+      if(st.has(s[i])==false)
+    {
+        {
+          st.add(s[i])
+        }
+      else
+        {
+          st.delete(s[i])
+        }
+    }
+  if(st.size<=1)
+  {
+    return true
+  }
+  else
+    {
+      return false
+    }
+}
+
+console.log(isPermutations())
