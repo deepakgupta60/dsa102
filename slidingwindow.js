8239955001e6e822cb07d0c575382a08843a7384
Deepak Gupta <deepakgupta60k@gmail.com>
2024-11-06 18:01:11 +0530

    working

--- slidingwindow.js
+++ slidingwindow.js
@@ -80,3 +80,49 @@ function slidingWindow()
   }  
   return ans
 }
+
+(function main(){
+  const ans = slidingWindow();
+  console.log("Answer: ", ans)
+}())
+
+function slidingWindow()
+{
+  let s = "abacd";
+  let k=3
+  
+  let left=0, right=0, ans=0
+  
+  let mp = new Map()
+  
+  while(right<s.length)
+  {
+    // console.log("test")
+    if(mp.has(s[right])==false)
+      {
+        mp.set(s[right], 1)
+      }
+    else
+      {
+        mp.set(s[right], mp.get(s[right])+1)
+      }
+    
+    while(mp.size>k)
+      {
+        mp.set(s[left], mp.get(s[left])-1)
+        
+        if(mp.get(s[left])==0)
+          {
+            mp.delete(s[left])
+          }
+        left++
+      }
+    
+    console.log(ans)
+    
+    ans=Math.max(ans, right-left+1)
+    
+    right++
+  }  
+  return ans
+}
