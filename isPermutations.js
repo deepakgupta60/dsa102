eb1ddffcdad7293d13480a47a87f4c70a608c3fc
Deepak Gupta <deepakgupta60k@gmail.com>
2024-11-07 00:23:07 +0530

    working

--- isPermutations.js
+++ isPermutations.js
@@ -1,26 +1,30 @@
-function isPermutations()
-{
-  let s="madam"
-  let st = new Set()
-  for(let i=0;i<s.length;i++)
-      if(st.has(s[i])==false)
-    {
-        {
-          st.add(s[i])
-        }
-      else
-        {
-          st.delete(s[i])
-        }
-    }
-  if(st.size<=1)
-  {
-    return true
-  }
-  else
-    {
-      return false
-    }
-}
+// function isPermutations()
+// {
+//   let s="madam"
+//   let st = new Set()
+//   for(let i=0;i<s.length;i++)
+//       if(st.has(s[i])==false)
+//     {
+//         {
+//           st.add(s[i])
+//         }
+//       else
+//         {
+//           st.delete(s[i])
+//         }
+//     }
+//   if(st.size<=1)
+//   {
+//     return true
+//   }
+//   else
+//     {
+//       return false
+//     }
+// }
 
-console.log(isPermutations())
+// console.log(isPermutations())
+
+// TC:0(n)
+// SC:0(1)
+
